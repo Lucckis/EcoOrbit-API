19c2af75feee3f24aeb6e4faeccbd7e14042359b
vinikenzo <kenzovini7@gmail.com>
2026-06-03 20:44:16 -0300

    implementação do filter

--- ecoorbit_api_usuario/src/main/java/com/ecoorbit/ecoorbit_api/config/SecurityConfig.java
+++ ecoorbit_api_usuario/src/main/java/com/ecoorbit/ecoorbit_api/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.ecoorbit.ecoorbit_api.config;
 
+import com.ecoorbit.ecoorbit_api.security.SecurityFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 
 import java.util.List;
@@ -15,11 +17,10 @@ import java.util.List;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(
-            HttpSecurity http
-    ) throws Exception {
+    private final SecurityFilter securityFilter;
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.configurationSource(request -> {
@@ -33,6 +34,7 @@ public class SecurityConfig {
                         .requestMatchers("/auth/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
